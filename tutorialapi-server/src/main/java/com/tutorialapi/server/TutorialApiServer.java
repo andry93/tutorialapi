4ce7c000f866205c0b84db065e979abdfda64c5d
andry <andry93@gmail.com>
2023-03-06 10:11:46 +0700

    added initial REST application and endpoint

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -1,6 +1,7 @@
 package com.tutorialapi.server;
 
 
+import com.tutorialapi.rest.ApiApplication;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -54,7 +55,7 @@ public class TutorialApiServer {
         server.setHandler(servletContextHandler);
 
         ServletHolder apiServletHolder = servletContextHandler.addServlet(ServletContainer.class,"/api/*");
-        apiServletHolder.setInitParameter("jakarta.ws.rs.Application","TODO");
+        apiServletHolder.setInitParameter("jakarta.ws.rs.Application", ApiApplication.class.getName());
 
         LOGGER.info("server starting");
         server.start();
